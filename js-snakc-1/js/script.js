5cad501251db2a3e31ed65625e574119c75a7018
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-04 17:50:04 +0200

    snack 1 incompleto

---
+++ js-snakc-1/js/script.js
@@ -0,0 +1,31 @@
+// 1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+const napoleon = [1, 2, 3, 4, 5, 6];
+const napoleonSecond = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let bestArray
+if (napoleon.length > napoleonSecond.length) {
+    bestArray = "napoleon è l'array più lungo";
+    do {
+        napoleonSecond.push(numberOneToUndred());
+    } while (napoleon.length > napoleonSecond.length);
+} else if (napoleon.length < napoleonSecond.length) {
+    bestArray = "napoleonSecond è l'array più lungo";
+    do {
+        napoleon.push(numberOneToUndred());
+    } while (napoleon.length < napoleonSecond.length);
+} else {
+    bestArray = "gli array sono della medesima lunghezza";
+}
+
+
+console.log(bestArray);
+
+console.log(napoleon);
+console.log(napoleonSecond);
+
+//funzione che genera un numero da 1 a 100 
+
+function numberOneToUndred() {
+    Math.floor(Math.random() * 100) + 1;
+}
